c7f31d6d74e9f83dfa889e44af066c310952d216
Pidbou <99590834+Pidbou@users.noreply.github.com>
2022-02-27 20:49:56 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+function SetL293in (num: number, num2: number, num3: number, num4: number) {
+    pins.digitalWritePin(DigitalPin.P3, num)
+    pins.digitalWritePin(DigitalPin.P4, num2)
+    pins.digitalWritePin(DigitalPin.P3, num3)
+    pins.digitalWritePin(DigitalPin.P3, num4)
+}
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Happy)
 })
@@ -13,8 +19,10 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . . . .
             . . . . .
             `)
+        SetL293in(0, 1, 0, 1)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
         basic.showIcon(IconNames.Happy)
+        SetL293in(0, 0, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
         basic.showLeds(`
             . . . . .
@@ -23,8 +31,10 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # . # .
             . . # . .
             `)
+        SetL293in(1, 0, 1, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
         basic.showIcon(IconNames.Happy)
+        SetL293in(0, 0, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -33,8 +43,10 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # . . .
             . . # . .
             `)
+        SetL293in(1, 0, 0, 1)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP) {
         basic.showIcon(IconNames.Happy)
+        SetL293in(0, 0, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -43,8 +55,10 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . . # .
             . . # . .
             `)
+        SetL293in(0, 1, 1, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_UP) {
         basic.showIcon(IconNames.Happy)
+        SetL293in(0, 0, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -88,3 +102,5 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
     }
 })
 basic.showIcon(IconNames.SmallHeart)
+pins.analogWritePin(AnalogPin.P1, 512)
+pins.analogWritePin(AnalogPin.P2, 512)
