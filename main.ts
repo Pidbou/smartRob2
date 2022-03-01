d2b2d411e3d5a9185ac2313bd833067d0d9cbb50
Pidbou <99590834+Pidbou@users.noreply.github.com>
2022-03-01 08:21:34 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,43 @@ function SetL293in (num: number, num2: number, num3: number, num4: number) {
     pins.digitalWritePin(DigitalPin.P5, num3)
     pins.digitalWritePin(DigitalPin.P6, num4)
 }
+function SetMotor2Speed (speed: number) {
+    if (speed < 0) {
+        pins.digitalWritePin(DigitalPin.P5, 1)
+        pins.digitalWritePin(DigitalPin.P6, 0)
+    } else if (speed > 0) {
+        pins.digitalWritePin(DigitalPin.P5, 0)
+        pins.digitalWritePin(DigitalPin.P6, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P5, 0)
+        pins.digitalWritePin(DigitalPin.P6, 0)
+    }
+    pins.analogWritePin(AnalogPin.P2, Math.round(Math.abs(speed) / 100 * 1023))
+}
+function SetMotor1Speed (speed: number) {
+    if (speed < 0) {
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        pins.digitalWritePin(DigitalPin.P4, 0)
+    } else if (speed > 0) {
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        pins.digitalWritePin(DigitalPin.P4, 0)
+    }
+    pins.analogWritePin(AnalogPin.P1, Math.round(Math.abs(speed) / 100 * 1023))
+}
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Happy)
 })
 bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
 })
+function VersArduino (Speed: number, MB1: number, MB2: number) {
+    pins.analogWritePin(AnalogPin.P0, Speed)
+    pins.digitalWritePin(DigitalPin.P1, MB1)
+    pins.digitalWritePin(DigitalPin.P2, MB2)
+}
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
         basic.showLeds(`
@@ -19,10 +50,10 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . . . .
             . . . . .
             `)
-        SetL293in(0, 1, 0, 1)
+        VersArduino(1000, 1, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
         basic.showIcon(IconNames.Happy)
-        SetL293in(0, 0, 0, 0)
+        VersArduino(0, 1, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
         basic.showLeds(`
             . . . . .
@@ -31,10 +62,10 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # . # .
             . . # . .
             `)
-        SetL293in(1, 0, 1, 0)
+        VersArduino(1000, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
         basic.showIcon(IconNames.Happy)
-        SetL293in(0, 0, 0, 0)
+        VersArduino(0, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -43,10 +74,10 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # . . .
             . . # . .
             `)
-        SetL293in(1, 0, 0, 1)
+        VersArduino(400, 1, 1)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP) {
         basic.showIcon(IconNames.Happy)
-        SetL293in(0, 0, 0, 0)
+        VersArduino(0, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -55,10 +86,10 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . . # .
             . . # . .
             `)
-        SetL293in(0, 1, 1, 0)
+        VersArduino(400, 0, 1)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_UP) {
         basic.showIcon(IconNames.Happy)
-        SetL293in(0, 0, 0, 0)
+        VersArduino(0, 0, 1)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -102,5 +133,4 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
     }
 })
 basic.showIcon(IconNames.SmallHeart)
-pins.analogWritePin(AnalogPin.P1, 512)
-pins.analogWritePin(AnalogPin.P2, 512)
+VersArduino(0, 0, 0)
