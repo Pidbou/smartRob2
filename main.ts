f9af55cb6ecf4900afbe6821462de0ddba04b626
Pidbou <99590834+Pidbou@users.noreply.github.com>
2022-03-07 10:43:30 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,48 +1,41 @@
 function SetL293in (num: number, num2: number, num3: number, num4: number) {
-    pins.digitalWritePin(DigitalPin.P3, num)
-    pins.digitalWritePin(DigitalPin.P4, num2)
-    pins.digitalWritePin(DigitalPin.P6, num3)
-    pins.digitalWritePin(DigitalPin.P7, num4)
+    pins.digitalWritePin(DigitalPin.P13, num)
+    pins.digitalWritePin(DigitalPin.P14, num2)
+    pins.digitalWritePin(DigitalPin.P15, num3)
+    pins.digitalWritePin(DigitalPin.P16, num4)
 }
 function SetMotor2Speed (speed: number) {
     if (speed < 0) {
-        pins.digitalWritePin(DigitalPin.P6, 1)
-        pins.digitalWritePin(DigitalPin.P7, 0)
+        pins.digitalWritePin(DigitalPin.P15, 1)
+        pins.digitalWritePin(DigitalPin.P16, 0)
     } else if (speed > 0) {
-        pins.digitalWritePin(DigitalPin.P6, 0)
-        pins.digitalWritePin(DigitalPin.P7, 1)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 1)
     } else {
-        pins.digitalWritePin(DigitalPin.P6, 0)
-        pins.digitalWritePin(DigitalPin.P7, 0)
+        pins.digitalWritePin(DigitalPin.P15, 0)
+        pins.digitalWritePin(DigitalPin.P16, 0)
     }
     pins.analogWritePin(AnalogPin.P2, Math.round(Math.abs(speed) / 100 * 1023))
 }
 function SetMotor1Speed (speed: number) {
     if (speed < 0) {
-        pins.digitalWritePin(DigitalPin.P3, 1)
-        pins.digitalWritePin(DigitalPin.P4, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.digitalWritePin(DigitalPin.P14, 0)
     } else if (speed > 0) {
-        pins.digitalWritePin(DigitalPin.P3, 0)
-        pins.digitalWritePin(DigitalPin.P4, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 1)
     } else {
-        pins.digitalWritePin(DigitalPin.P3, 0)
-        pins.digitalWritePin(DigitalPin.P4, 0)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.digitalWritePin(DigitalPin.P14, 0)
     }
     pins.analogWritePin(AnalogPin.P1, Math.round(Math.abs(speed) / 100 * 1023))
 }
 bluetooth.onBluetoothConnected(function () {
-    pins.digitalWritePin(DigitalPin.P9, 1)
     basic.showIcon(IconNames.Happy)
 })
 bluetooth.onBluetoothDisconnected(function () {
-    pins.digitalWritePin(DigitalPin.P9, 0)
     basic.showIcon(IconNames.Sad)
 })
-function VersArduino (Speed: number, MB1: number, MB2: number) {
-    pins.analogWritePin(AnalogPin.P0, Speed)
-    pins.digitalWritePin(DigitalPin.P1, MB1)
-    pins.digitalWritePin(DigitalPin.P2, MB2)
-}
 control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EVT_ANY, function () {
     if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_DOWN) {
         basic.showLeds(`
@@ -52,10 +45,8 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . . . .
             . . . . .
             `)
-        VersArduino(1000, 1, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_A_UP) {
         basic.showIcon(IconNames.Happy)
-        VersArduino(0, 1, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_DOWN) {
         basic.showLeds(`
             . . . . .
@@ -64,10 +55,8 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # . # .
             . . # . .
             `)
-        VersArduino(1000, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_B_UP) {
         basic.showIcon(IconNames.Happy)
-        VersArduino(0, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -76,10 +65,8 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . # . . .
             . . # . .
             `)
-        VersArduino(400, 1, 1)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_C_UP) {
         basic.showIcon(IconNames.Happy)
-        VersArduino(0, 0, 0)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -88,10 +75,8 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
             . . . # .
             . . # . .
             `)
-        VersArduino(400, 0, 1)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_D_UP) {
         basic.showIcon(IconNames.Happy)
-        VersArduino(0, 0, 1)
     } else if (control.eventValue() == EventBusValue.MES_DPAD_BUTTON_1_DOWN) {
         basic.showLeds(`
             . . # . .
@@ -134,7 +119,4 @@ control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MICROBIT_EV
         basic.showIcon(IconNames.Happy)
     }
 })
-led.enable(false)
 basic.showIcon(IconNames.SmallHeart)
-VersArduino(0, 0, 0)
-pins.digitalWritePin(DigitalPin.P9, 0)
